5f06d27af6b24a4b0661260e419378777431db26
u1anov <59798390+u1anov@users.noreply.github.com>
2020-01-20 20:29:34 +0300

    добавлены картинки

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-alert()
+let castle = document.getElementById('castle');
+let target = document.getElementById('target');
+let lvl = document.getElementById('lvl');
+let score = 0;
+
+castle.addEventListener('click',function(){
+    score++;
+    lvl.innerHTML = score;
+});
